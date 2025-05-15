d4be54d491a5e692c9d61164d4ab88ad4115865e
gaspardvilla <37636524+gaspardvilla@users.noreply.github.com>
2025-05-15 10:17:05 +0200

    exercices

--- generator.py
+++ generator.py
@@ -3,13 +3,14 @@ import numpy as np
 from generators.revenues import generate_revenue_dataset
 from generators.time_series import generate_brownian_motion
 from generators.houses import generate_houses_dataset
-from generators.ci import generate_ci ###################
+from generators.ci import generate_ci
+from generators.icecreams import ice_cream_sales_drowning_deaths
 
 if __name__ == '__main__':
-
     # Fix the path and the seed
     seed = 47689
-    path = '/Users/gaspardvilla/Local/NAT/nat_ada/data'
+    np.random.seed(seed)
+    path = 'data'
 
     # Create the data folder if it doesn't exist
     os.makedirs(path, exist_ok=True)
@@ -18,4 +19,5 @@ if __name__ == '__main__':
     generate_revenue_dataset(1000, seed, path)
     generate_brownian_motion(seed, path)
     generate_houses_dataset(3000, seed, path)
-    generate_ci(path)       ###############
+    generate_ci(path)
+    ice_cream_sales_drowning_deaths(path)
